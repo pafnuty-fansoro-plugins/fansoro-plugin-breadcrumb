3d48f6c76b999da175a2c99de68f83fd960a1eda
Pavel Belousov <pafnuty10@gmail.com>
2015-11-22 00:34:25 +0300

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# morfy-plugin-breadcrumb
-Breadcrumb Plugin for Morfy CMS
+# Breadcrumb Plugin for [Morfy CMS](http://morfy.org/)
+
+![version](https://img.shields.io/badge/version-1.0.0-brightgreen.svg?style=flat-square "Version")
+![DLE](https://img.shields.io/badge/Morfy-2.x-green.svg?style=flat-square "Morfy Version")
+[![MIT License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)](https://github.com/pafnuty-morfy-plugins/morfy-plugin-breadcrumb/blob/master/LICENSE)
+
+The plugin to create breadcrumbs for site.
+
+
+
+## Advantages
+- Easy use.
+- SEO-frendly template.
+
+
+## Install
+Copy the content of folder `upload` in the root of the site.
+If you use custom template copy the folder `/upload/themes/default/plugins/` folder with your template.
+
+## Configuration
+- Open `/plugins/breadcrumb/breadcrumb.yml` and configure the settings.
+```yml
+# List of item's readable text or labels
+labels:
+  home: Main
+```
+
+## Usage
+```smarty
+{Action::run('breadcrumb')}
+```
+
+## License 
+[MIT](https://github.com/pafnuty/morfy-less/blob/master/LICENSE)
---
+++ upload/plugins/breadcrumb/breadcrumb.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Breadcrumb Plugin
+ * Based on idea from https://github.com/tovic/breadcrumb-plugin-for-morfy-cms
+ *
+ * @package Morfy
+ * @subpackage Plugins
+ * @author Pavel Belousov / pafnuty
+ * @version 1.0.0
+ * @license https://github.com/pafnuty-morfy-plugins/morfy-plugin-breadcrumb/blob/master/LICENSE MIT
+ */
+
+Action::add('breadcrumb', function () {
+
+	// Configuration data
+	$config = Config::get('plugins.breadcrumb');
+	// Get current URI segments
+	$paths = Url::getUriSegments();
+	// Count total paths
+	$total_paths = count($paths);
+	// Path lifter
+	$lift = '';
+	// Breadcrumb's data
+	$data = [];
+
+	for ($i = 0; $i < $total_paths; $i++) {
+		$lift .= '/' . $paths[$i];
+		$page = Pages::getPage(file_exists(STORAGE_PATH . '/pages/' . $lift . '/index.md') || file_exists(STORAGE_PATH . '/pages/' . $lift . '.md') ? $lift : '404');
+
+		$data[Url::getBase() . $lift] = [
+			'title'   => $page['title'],
+			'current' => rtrim(Url::getCurrent(), '/') === rtrim(Url::getBase() . $lift, '/'),
+		];
+	}
+	$template = Template::factory(THEMES_PATH . '/' . Config::get('system.theme'));
+
+	$template->display(
+		'/plugins/breadcrumb/breadcrumb.tpl',
+		[
+			'home'   => rtrim(Url::getCurrent(), '/') === rtrim(Url::getBase(), '/') ? true : Url::getBase(),
+			'config' => $config,
+			'branch' => $data,
+		]
+	);
+
+});
